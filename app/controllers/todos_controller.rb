c6d3697197675a518a484a84fa09aa0372f4d5ca
tob <tobiadonati@gmail.com>
2017-07-13 10:56:18 +0200

    Adding views and styling

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,6 +5,7 @@ class TodosController < ApplicationController
   # GET /todos.json
   def index
     @todos = Todo.all
+    @todo = Todo.new
   end
 
   # GET /todos/1
@@ -28,10 +29,10 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to todos_path, notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
-        format.html { render :new }
+        format.html { redirect_to todos_path }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
     end
