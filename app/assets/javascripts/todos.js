486d79c100e8c64c33b3f1080f56438c311c81d1
tob <tobiadonati@gmail.com>
2017-07-13 12:12:51 +0200

    Create Todos via AJAX

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -69,19 +69,8 @@ function createTodo(title) {
     console.log(error)
     error_message = error.responseJSON.title[0];
     showError(error_message);
-  })
-
-  function showError(message) {
-    var errorHelpBlock = $('<span class="help-block"></span>')
-      .attr('id', 'error_message')
-      .text(message);
-
-    $("#formgroup-title")
-      .addClass("has-error")
-      .append(errorHelpBlock);
-  }
+  });
 }
-
 
 function resetErrors() {
   $("#error_message").remove();
